bca2f58b162e2e4b6eb9d874d589a34c50db5858
W4118 Student <yw2676@columbia.edu>
2014-10-08 17:16:37 -0400

    rm all the other 4 syscall files

--- flo-kernel/arch/arm/kernel/accevt_destroy.c
+++
@@ -1,5 +0,0 @@
-#include<linux/syscalls.h>
-asmlinkage long sys_accevt_destroy(int event_id)
-{
-	return 0;
-}
--- flo-kernel/arch/arm/kernel/accevt_signal.c
+++
@@ -1,6 +0,0 @@
-#include<linux/syscalls.h>
-asmlinkage long sys_accevt_signal(struct dev_acceleration __user * acceleration)
-{
-	return 0;
-
-}
