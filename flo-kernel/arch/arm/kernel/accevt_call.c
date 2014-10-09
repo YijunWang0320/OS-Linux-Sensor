9388e2986c8811f66ae39e348ce2857d70556a63
Wei Dai <wdai2248@columbia.edu>
2014-10-08 21:18:48 -0400

    system call accevt_destroy() implement. Need to add lock later

--- flo-kernel/arch/arm/kernel/accevt_call.c
+++ flo-kernel/arch/arm/kernel/accevt_call.c
@@ -1,14 +1,23 @@
 #include<linux/syscalls.h>
 #include<linux/uaccess.h>
 #include<linux/slab.h>
+#include<linux/kfifo.h>
+#include<linux/wait.h>
 #define ACC_M struct acc_motion
-static struct acc_motion  group[20];
+struct event_unit {
+	ACC_M mBaseline;
+	wait_queue_head_t mWaitQueue;
+	int event_id;
+	int event_count; 
+	struct event_unit *next;
+};
+static struct event_unit *event_list;
 static int motion_count = 0;
+
 asmlinkage long sys_accevt_create(struct acc_motion __user *acceleration)
 {
 	ACC_M *tmpAcc;
 	int ret = 0;
-	int i=0;
 
 	if(acceleration == NULL)
 		return -1;
@@ -16,14 +25,30 @@ asmlinkage long sys_accevt_create(struct acc_motion __user *acceleration)
 	ret = copy_from_user(tmpAcc, acceleration, sizeof(ACC_M));
 	if (ret != 0)
 		return -1;
-	group[motion_count] = *tmpAcc;
 	motion_count++;
-	if(motion_count == 10){
-		for(i = 0; i<10; i++){
-			printk("motion count%d:%d\n",i,group[i].frq);
+	struct event_unit *new_event;
+	new_event = (struct event_unit *)kmalloc(sizeof(struct event_unit),GFP_KERNEL);
+	new_event->event_id = motion_count;
+	new_event->event_count = 1;
+	new_event->mBaseline = *tmpAcc;
+	new_event->next = NULL;
+	
+	if (event_list==NULL) {
+		event_list=new_event;
+	} else {
+		new_event->next = event_list;
+		event_list = new_event;	
+	}
+	if (motion_count==10) {
+		struct event_unit * p = event_list;
+		while(p!= NULL) {
+			printk("event: eventid=%d",p->event_id);
+			p = p->next;
 		}
 	}
-	return 0;
+	
+	kfree(tmpAcc);
+	return motion_count;
 }
 asmlinkage long sys_accevt_wait(int event_id)
 {
@@ -35,5 +60,32 @@ asmlinkage long sys_accevt_signal(struct dev_acceleration __user *acceleration)
 }
 asmlinkage long sys_accevt_destroy(int event_id)
 {
+	struct event_unit *p, *pre;
+
+	p = event_list;
+	pre = NULL;
+	while (p != NULL) {
+		if (p->event_id == event_id)
+			break;
+		else {
+			pre = q;
+			p = p->next;
+		}
+	}
+	if (p == NULL) 
+		return -1;
+	if (p->event_count > 1)
+		p->event_count--;
+	else {
+		pre->next = p->next;
+		int status;
+
+		status = kfree(p);
+		if (status < 0)
+			return -1;
+	}
+
+
+
 	return 0;
 }
